105523bccbf16ced1e7026387cdbaa943f3c40d7
mateusmosquera <mateusmosquera@hotmail.com>
2021-05-20 16:51:59 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/mateus/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.mateus.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
